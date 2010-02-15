78f7e59cc662cede6984be204e4133e87f97a4f8
tobias <tobias@dab74e7a-dce9-0310-a56b-ef9c8ba74961>
2010-02-15 12:53:55 +0000

    git-svn-id: file:///home/tobias/.svn/repos/airspace/trunk@99 dab74e7a-dce9-0310-a56b-ef9c8ba74961

--- src/main/java/de/gliderpilot/AirspaceIgcImageCreator.java
+++ src/main/java/de/gliderpilot/AirspaceIgcImageCreator.java
@@ -14,19 +14,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
 
 import de.gliderpilot.airspace.AirspaceVector;
 import de.gliderpilot.airspace.OpenAirspaceFile;
 import de.gliderpilot.geom.Point4D;
 import de.gliderpilot.geom.Rectangle4D;
 import de.gliderpilot.igc.IgcFile;
-
 import de.gliderpilot.tracklog.TrackLog;
 import de.gliderpilot.util.Util;
 
@@ -37,118 +37,133 @@ import de.gliderpilot.util.Util;
  */
 public class AirspaceIgcImageCreator {
 
-    static boolean antialise = false;
+	static boolean antialise = false;
 
-    AirspaceVector airspace;
+	AirspaceVector airspace;
 
-    TrackLog track;
+	TrackLog track;
 
-    /**
-     * Creates a new AirspaceIgcImageCreator object.
-     * 
-     * @param airFile
-     *            DOCUMENT ME!
-     * @param igcFile
-     *            DOCUMENT ME!
-     */
-    public AirspaceIgcImageCreator(File airFile, String igcFile) {
-        Logger.getLogger(getClass()).setLevel(Level.INFO);
-        airspace = new AirspaceVector();
-        track = new TrackLog();
+	/**
+	 * Creates a new AirspaceIgcImageCreator object.
+	 * 
+	 * @param airFile
+	 *            DOCUMENT ME!
+	 * @param igcFile
+	 *            DOCUMENT ME!
+	 */
+	public AirspaceIgcImageCreator(File airFile, String igcFile) {
+		Logger.getLogger(getClass()).setLevel(Level.INFO);
+		airspace = new AirspaceVector();
+		track = new TrackLog();
 
-        if (!(new OpenAirspaceFile(airFile, airspace).read())) {
-            System.out.println("Could not parse airspaceFile");
-            System.exit(2);
-        }
+		if (!(new OpenAirspaceFile(airFile, airspace).read())) {
+			System.out.println("Could not parse airspaceFile");
+			System.exit(2);
+		}
 
-        if (!(new IgcFile(igcFile, track).parse())) {
-            System.out.println("Could not parse igcFile");
-            System.exit(3);
-        }
-    }
+		if (!(new IgcFile(igcFile, track).parse())) {
+			System.out.println("Could not parse igcFile");
+			System.exit(3);
+		}
+	}
 
-    /**
-     * DOCUMENT ME!
-     * 
-     * @param fileName
-     *            DOCUMENT ME!
-     * 
-     * @return DOCUMENT ME!
-     */
-    public boolean createTrackImage(String fileName) {
-        BufferedImage bi = new BufferedImage(400, 400,
-                BufferedImage.TYPE_INT_RGB);
-        Graphics g = bi.getGraphics();
-        Graphics2D g2d = (Graphics2D) g;
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @param fileName
+	 *            DOCUMENT ME!
+	 * 
+	 * @return DOCUMENT ME!
+	 */
+	public boolean createTrackImage(String fileName) {
+		Iterator<ImageWriter> imageWriters = ImageIO
+				.getImageWritersByFormatName("jpeg");
+		if (!imageWriters.hasNext()) {
+			return false;
+		}
+		FileOutputStream fo = null;
 
-        if (antialise) {
-            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                    RenderingHints.VALUE_ANTIALIAS_ON);
-        }
+		try {
+			ImageWriter imageWriter = imageWriters.next();
+			fo = new FileOutputStream(fileName);
+			imageWriter.setOutput(fo);
+			BufferedImage bi = new BufferedImage(400, 400,
+					BufferedImage.TYPE_INT_RGB);
+			Graphics g = bi.getGraphics();
+			Graphics2D g2d = (Graphics2D) g;
 
-        FileOutputStream fo;
+			if (antialise) {
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_ON);
+			}
 
-        try {
-            fo = new FileOutputStream(fileName);
-        } catch (FileNotFoundException e) {
-            return false;
-        }
+			Rectangle4D rect = track.getBounds4D();
+			Point4D center = new Point4D(rect.getCenterX(), rect.getY());
 
-        JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(fo);
+			Point4D ul = new Point4D(rect.getX(), rect.getY());
+			double zoom = Util.getFactor(bi.getWidth(), 2 * center
+					.getXDistanceFrom(ul));
+			double zoom2 = Util.getFactor(bi.getHeight(), 2 * center
+					.getYDistanceFrom(ul));
+			zoom = (zoom < zoom2) ? zoom : zoom2;
 
-        Rectangle4D rect = track.getBounds4D();
-        Point4D center = new Point4D(rect.getCenterX(), rect.getY());
+			AffineTransform xform = new AffineTransform();
+			xform.setToScale(zoom * center.getXCorrection(null), -zoom);
+			xform.translate(-rect.getX(), -rect.getY() - rect.getHeight());
 
-        Point4D ul = new Point4D(rect.getX(), rect.getY());
-        double zoom = Util.getFactor(bi.getWidth(), 2 * center
-                .getXDistanceFrom(ul));
-        double zoom2 = Util.getFactor(bi.getHeight(), 2 * center
-                .getYDistanceFrom(ul));
-        zoom = (zoom < zoom2) ? zoom : zoom2;
+			g.setColor(Color.white);
+			g.fillRect(0, 0, bi.getWidth(), bi.getHeight());
+			airspace.draw(g, rect, xform);
+			track.draw(g, rect, xform);
 
-        AffineTransform xform = new AffineTransform();
-        xform.setToScale(zoom * center.getXCorrection(null), -zoom);
-        xform.translate(-rect.getX(), -rect.getY() - rect.getHeight());
+			imageWriter.write(bi);
+			return true;
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (fo != null) {
+				try {
+					fo.flush();
+					fo.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return false;
+	}
 
-        g.setColor(Color.white);
-        g.fillRect(0, 0, bi.getWidth(), bi.getHeight());
-        airspace.draw(g, rect, xform);
-        track.draw(g, rect, xform);
+	/**
+	 * DOCUMENT ME!
+	 */
+	public static void help() {
+		System.out
+				.println("Arguments must be: openAirspaceFile igcFile imageFile [-antialise]");
+		System.exit(1);
+	}
 
-        encoder.encode(bi);
+	/**
+	 * DOCUMENT ME!
+	 * 
+	 * @param args
+	 *            DOCUMENT ME!
+	 */
+	public static void main(String[] args) {
+		if (args.length < 3) {
+			help();
+		}
 
-        return true;
-    }
+		if (args.length == 4) {
+			antialise = true;
+		}
 
-    /**
-     * DOCUMENT ME!
-     */
-    public static void help() {
-        System.out
-                .println("Arguments must be: openAirspaceFile igcFile imageFile [-antialise]");
-        System.exit(1);
-    }
-
-    /**
-     * DOCUMENT ME!
-     * 
-     * @param args
-     *            DOCUMENT ME!
-     */
-    public static void main(String[] args) {
-        if (args.length < 3) {
-            help();
-        }
-
-        if (args.length == 4) {
-            antialise = true;
-        }
-
-        AirspaceIgcImageCreator imageCreator;
-        long millis = System.currentTimeMillis();
-        imageCreator = new AirspaceIgcImageCreator(new File(args[0]), args[1]);
-        imageCreator.createTrackImage(args[2]);
-        System.out.println("Image created. lasted "
-                + (System.currentTimeMillis() - millis) + " ms.");
-    }
+		AirspaceIgcImageCreator imageCreator;
+		long millis = System.currentTimeMillis();
+		imageCreator = new AirspaceIgcImageCreator(new File(args[0]), args[1]);
+		imageCreator.createTrackImage(args[2]);
+		System.out.println("Image created. lasted "
+				+ (System.currentTimeMillis() - millis) + " ms.");
+	}
 }
